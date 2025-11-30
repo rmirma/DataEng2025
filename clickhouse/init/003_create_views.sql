3983fab07af96692461915a7b34aa3820b629d39
Doris <doris.kaambre@gmail.com>
2025-11-30 21:38:33 +0200

    create roles

--- clickhouse/init/003_create_views.sql
+++ clickhouse/init/003_create_views.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS views;
 
-
-CREATE OR REPLACE VIEW views.VotingBase AS
+CREATE OR REPLACE VIEW views.Voting AS
 SELECT
     VotingId,
     VotingSrcId,
@@ -17,6 +16,24 @@ SELECT
     Against,
     Neutral,
     Abstained
+FROM gold.Voting;
+
+CREATE OR REPLACE VIEW views.VotingPseudo AS
+SELECT
+    VotingId,
+    VotingSrcId,
+    VotingType,
+    WeatherId,
+    Date,
+    StartTime,
+    EndTime,
+    VotingDescription,
+    Present,
+    Absent,
+    SHA256(toString(InFavour)) AS InFavour,
+    SHA256(toString(Against)) AS Against,
+    SHA256(toString(Neutral)) AS Neutral,
+    SHA256(toString(Abstained)) AS Abstained
 FROM gold.Voting;
 
 
@@ -52,6 +69,47 @@ SELECT
     v.Against,
     v.Neutral,
     v.Abstained
+FROM gold.Voting AS v
+LEFT JOIN gold.DimVotingType AS vt
+    ON v.VotingType = vt.VotingType
+LEFT JOIN gold.DimWeather AS w
+    ON v.WeatherId = w.WeatherId
+LEFT JOIN gold.DimDate AS d
+    ON v.Date = d.Date;
+
+
+CREATE OR REPLACE VIEW views.VotingAnalyticsPseudo AS
+SELECT
+    v.VotingId,
+    v.VotingSrcId,
+    v.VotingType,
+    vt.VotingTypeDesc,
+    v.WeatherId,
+    w.StartTime     AS WeatherStartTime,
+    w.EndTime       AS WeatherEndTime,
+    w.AvgTemp,
+    w.HighTemp,
+    w.LowTemp,
+    w.AvgPrecipitation,
+    w.MaxPrecipitation,
+    w.AvgWindSpeed,
+    w.MaxWindSpeed,
+    v.Date,
+    d.WeekDay,
+    d.HolidayInd,
+    d.HolidayDesc,
+    d.Year,
+    d.Month,
+    d.Season,
+    v.StartTime,
+    v.EndTime,
+    v.VotingDescription,
+    v.Present,
+    v.Absent,
+    SHA256(toString(InFavour)) AS InFavour,
+    SHA256(toString(Against)) AS Against,
+    SHA256(toString(Neutral)) AS Neutral,
+    SHA256(toString(Abstained)) AS Abstained
 FROM gold.Voting AS v
 LEFT JOIN gold.DimVotingType AS vt
     ON v.VotingType = vt.VotingType
---
+++ clickhouse/init/004_create_roles.sql
@@ -0,0 +1,11 @@
+CREATE ROLE analyst_full;
+CREATE ROLE analyst_limited;
+
+CREATE USER user_full IDENTIFIED WITH plaintext_password BY 'user_full';
+CREATE USER user_limited IDENTIFIED WITH plaintext_password BY 'user_limited';
+
+GRANT analyst_full TO user_full;
+GRANT analyst_limited TO user_limited;
+
+GRANT SELECT ON views.Voting, views.VotingAnalytics TO analyst_full;
+GRANT SELECT ON views.VotingPseudo, views.VotingAnalyticsPseudo TO analyst_limited;
